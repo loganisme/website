8f26a57c1cb130bac2746ea491ab43a49eed7ba7
loganisme <ngoanhtue312005@gmail.com>
2021-12-10 17:47:45 +0700

    up

--- index.js
+++ index.js
@@ -1,52 +1,73 @@
-// ex1
-function findEven(number) {
-    for (let i = 4; i <= number; i++) {
-      if (i % 2 == 0) {
-        console.log(i);
-      }
-    }
+let lyrics = `Nụ cười của Super Idol
+Cũng không ngọt như cậu
+Cái nắng chói chang giữa tháng 8
+Cũng không tỏa nắng bằng cậu
+Nhiệt tâm 105°C của cậu 
+Như từng giọt nước cất thuần khiết
+Cậu nào biết cậu đáng yêu nhường nào
+Sau khi cậu té cậu lại đứng lên cười ngây ngô
+Trước giờ cậu chưa từng dễ dàng chịu thất bại
+Cậu luôn kiên quyết theo đuổi giấc mơ mà chưa từng thay đổi
+Rất yên tâm khi cậu nói với tớ
+Chớ sợ có tớ đây
+Cứ giao hết cho tớ
+Dũng cảm theo đuổi giấc mơ
+Dáng vẻ kiên định ấy
+Nụ cười của Super Idol cũng không ngọt như cậu
+Cái nắng chói chang giữa tháng 8
+Cũng không tỏa nắng bằng cậu
+Nhiệt tâm 105°C của cậu
+Như từng giọt nước cất thuần khiết
+Nơi đây độc nhất vô nhị
+Thời đại thuộc về tớ
+Chẳng sợ thất bại cứ thử một lần
+Sảng khoái nhiệt tình yêu thương
+Nhiệt tâm 105°C của cậu
+Như từng giọt nước cất thuần khiết
+Nơi đây độc nhất vô nhị
+Thời đại thuộc về tớ
+Chẳng sợ thất bại cứ thử một lần
+Sảng khoái nhiệt tình yêu thương
+Nhiệt tâm 105°C của cậu
+Như từng giọt nước cất thuần khiết
+Uống một hớp lại một hớp, sức sống căng tràn
+Lần nữa trở về trạng thái tốt nhất
+Cậu nào biết cậu đáng yêu nhường nào
+Sau khi cậu té cậu lại đứng lên cười ngây ngô
+Trước giờ cậu chưa từng dễ dàng chịu thất bại
+Cậu luôn kiên quyết theo đuổi giấc mơ mà chưa từng thay đổi
+Rất yên tâm khi cậu nói với tớ
+Chớ sợ có tớ đây
+Cứ giao hết cho tớ
+ Dũng cảm theo đuổi giấc mơ
+Dáng vẻ kiên định ấy
+Nụ cười của Super Idol cũng không ngọt như cậu
+Cái nắng chói chang giữa tháng 8
+Cũng không tỏa nắng bằng cậu
+Nhiệt tâm 105°C của cậu
+Như từng giọt nước cất thuần khiết
+Nơi đây độc nhất vô nhị
+Thời đại thuộc về tớ
+Chẳng sợ thất bại cứ thử một lần
+Sảng khoái nhiệt tình yêu thương
+Nhiệt tâm 105°C của cậu
+Như từng giọt nước cất thuần khiết
+Nơi đây độc nhất vô nhị
+Thời đại thuộc về tớ
+Chẳng sợ thất bại cứ thử một lần
+Sảng khoái nhiệt tình yêu thương
+Như từng giọt nước cất thuần khiết
+Uống một hớp lại một hớp, sức sống căng tràn
+Lần nữa trở về trạng thái tốt nhất
+Uống một hớp lại một hớp, sức sống căng tràn.`;
+let newly = lyrics.split("\n");
+var c = document.getElementById("nhac").innerHTML =""
+for (let i = 1000,a = 0; i < newly.length*1000,a<newly.length; i += 1000,a++) {
+  function run() { 
+    document.getElementById("nhac").innerHTML += newly[a] + "<br>";  ;
   }
-  
-  // ex2
-  let div = document.getElementById("bai2");
-  function changeColor() {
-    div.style.backgroundColor = "black";
-  }
-  function changeToPink() {
-    div.style.backgroundColor = "pink";
-  }
-  
-  // ex3
-  function numberOneTriangle() {
-    for (let a = 0; a < 5; a++) {
-      for (let b = 0; b <= a; b++) {
-        document.writeln(1);
-      }
-      document.writeln("<br>");
-    }
-  }
-  
-  // ex4
-  
-  function clock() {
-    let gio = document.getElementById("hour");
-    let phut = document.getElementById("minutes");
-    let giay = document.getElementById("seconds");
-    let demgio = new Date().getHours();
-    let demphut = new Date().getMinutes();
-    let demgiay = new Date().getSeconds();
-    gio.innerHTML = demgio + " : ";
-    phut.innerHTML = demphut + " : ";
-    giay.innerHTML = demgiay;
-    if (demgio < 10) {
-      gio.innerHTML = "0" + demgio + " : ";
-    }
-    if (demphut < 10) {
-      phut.innerHTML = "0" + demphut + " : ";
-    }
-    if (demgiay < 10) {
-      giay.innerHTML = "0" + demgiay;
-    }
-  }
-  setInterval("clock()", 1000);
-  
+  setTimeout(run, i)
+  console.log(i);
+  console.log(a);
+}
+alert("bắt đầu");
